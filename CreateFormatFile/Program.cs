6c2bd3e545f636da27a90ed3b46711e3c126ae3a
Edmund Warren <blindeddie@optonline.net>
2019-01-06 20:22:25 -0500

    initial Commit

---
+++ CreateFormatFile/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: blind
+ * Date: 1/6/2019
+ * Time: 8:20 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace CreateFormatFile
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
